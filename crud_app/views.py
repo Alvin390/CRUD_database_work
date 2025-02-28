86e68d0f473394906c3ceae108f8c2507fedfac1
Alvin390 <alvinkariuki07@gmail.com>
2025-02-28 13:07:18 +0300

    CRUD_database

--- crud_app/views.py
+++ crud_app/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Crud
-
-
+from .forms import EntryForm
 # Create your views here.
-def index (request):
-    return render (request,'index.html')
+def entry_list(request):
+    entry=Crud.objects.all()
+    return render(request,'listingpage.html',{'entry':entry})
